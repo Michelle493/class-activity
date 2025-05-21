7fcc9b25092026a8ae22fca7b86df72ab9cbf201
Michelle Odusanwo <m.odusanwo@alustudent.com>
2025-05-21 01:59:25 -0700

    activity

---
+++ FirstScript.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+Name=Diana
+echo "$Name"
+
+math=$((5*6+2/2))
+echo  "$math"
+name="Michelle"
+echo "Hello, $name!"
+
+echo "What is your name?"
+read name
+echo "Hello, $name! Welcome to shell scripting."
